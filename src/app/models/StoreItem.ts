6001fe1840df4b00a4e6e4de703d25e45495ab2d
maksymilian-lakomy <zommer128@gmail.com>
2020-02-05 23:38:31 +0100

    Various style changes

--- src/app/models/StoreItem.ts
+++ src/app/models/StoreItem.ts
@@ -4,12 +4,14 @@ export class StoreItem {
     name: string;
     description: string;
     price: number;
+    gallery: string[];
 
-    constructor(id: number, category: string[], name: string, description: string, price: number) {
+    constructor(id: number, category: string[], name: string, description: string, price: number, gallery: string[]) {
         this.id = id;
         this.category = category;
         this.name = name;
         this.description = description;
         this.price = price;
+        this.gallery = gallery;
     }
 }
